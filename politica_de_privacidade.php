3838be957444b0b55b010d81f0df925c31b2d860
Tales Macedo <talesmgbahia@hotmail.com>
2025-02-24 15:57:14 -0300

    alterando layout das divs das politicas de privacidade

--- politica_de_privacidade.php
+++ politica_de_privacidade.php
@@ -31,7 +31,7 @@
             </div>
         </section>
 
-        <section id="privacy-policy" class="privacy-policy section">
+        <section id="services" class="services section">
 
             <div class="container">
 
@@ -42,118 +42,125 @@
                 </div>
                 <div class="row gy-4">
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item  position-relative">
+                <div class="service-item  position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                            <h3>1. Coleta e Uso de Informações</h3>
+                        
+                        </div>
 
-                    <h3>1. Coleta e Uso de Informações</h3>
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">                        
 
+                            <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
+                        
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
                     
-                    </div>
-                </div>
-
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <h3>2. Retenção e Segurança dos Dados</h3>
+                            <h3>2. Retenção e Segurança dos Dados</h3>
                     
-                    </div>
-                </div>
+                        </div>
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas, roubos, acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
                     
-                    </div>
-                </div>
-
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <h3>3. Compartilhamento de Dados</h3>
-                                        
-                    </div>
-                </div>
-
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
+                            <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas, roubos, acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
                     
+                        </div>
+                    </div>
+                </div>
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+                            <h3>3. Compartilhamento de Dados</h3>
+                                                
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+
+                            <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
+                            
+                        </div>
                     </div>
                 </div> 
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
 
-                    <h3>4. Links para Sites Externos</h3>
-                                        
-                    </div>
-                </div>
+                            <h3>4. Links para Sites Externos</h3>
+                                            
+                        </div>
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <p>Nosso site pode conter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
-                    
-                    </div>
-                </div> 
-
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <h3>5. Cookies e Publicidade</h3>
-                                        
-                    </div>
-                </div>
-
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-
-                    <p>O serviço Google AdSense que usamos para veicular publicidade utiliza um cookie DoubleClick para exibir anúncios mais relevantes e limitar a frequência de exibição. Para mais informações, consulte as FAQs oficiais sobre privacidade do Google AdSense.</p>
-                    
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                            <p>Nosso site pode conter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
+                        
+                        </div>
                     </div>
                 </div> 
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+                            <h3>5. Cookies e Publicidade</h3>                         
+                            
+                        </div>
 
-                    <h3>6. Compromisso do Usuário</h3>
-                                        
-                    </div>
-                </div>
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-                    <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Maktub Promotora oferece no site e não se envolver em atividades ilegais ou contrárias à boa fé e ordem pública.</p>
-                    <ul>
-                        <li>Não se envolver em atividades ilegais;</li>
-                        <li>Não difundir propaganda ou conteúdo ofensivo;</li>
-                        <li>Não causar danos aos sistemas físicos e lógicos do site.</li>
-                    </ul>
+                            <p>O serviço Google AdSense que usamos para veicular publicidade utiliza um cookie DoubleClick para exibir anúncios mais relevantes e limitar a frequência de exibição. Para mais informações, consulte as FAQs oficiais sobre privacidade do Google AdSense.</p>
+                        
+                        </div>
                     </div>
                 </div> 
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
 
-                    <h3>7. Mais Informações</h3>
-                                        
+                            <h3>6. Compromisso do Usuário</h3>
+                                                
+                            
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+                            <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Maktub Promotora oferece no site e não se envolver em atividades ilegais ou contrárias à boa fé e ordem pública.</p>
+                            <ul>
+                                <li>Não se envolver em atividades ilegais;</li>
+                                <li>Não difundir propaganda ou conteúdo ofensivo;</li>
+                                <li>Não causar danos aos sistemas físicos e lógicos do site.</li>
+                            </ul>
+
+                        </div>
                     </div>
-                </div>
+                </div> 
 
-                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
 
-                    <p>Se houver dúvidas sobre nossa política de privacidade, entre em contato conosco.</p>
-                    
+                            <h3>7. Mais Informações</h3>
+                                                
+                            
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+
+                            <p>Se houver dúvidas sobre nossa política de privacidade, entre em contato conosco.</p>
+                            
+                        </div>
                     </div>
                 </div> 
 
@@ -161,46 +168,6 @@
 
             </div>
 
-            <!--
-            <div class="container position-relative" data-aos="fade-up" data-aos-delay="10">
-                <div class="content text-center">
-                    <h2>Política de Privacidade</h2>
-                    
-                    <p>A sua privacidade é importante para nós. É política do Maktub Promotora respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site Maktub Promotora, e outros sites que possuímos e operamos.</p>
-                </div>
-                
-                <div class="content">
-                    <div class="col-lg-6">
-                    <h3>1. Coleta e Uso de Informações</h3>
-                    </div>
-                    <div class="col-lg-6">
-                    <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
-                    </div>
-                    <h3>2. Retenção e Segurança dos Dados</h3>
-                    <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas, roubos, acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
-                    
-                    <h3>3. Compartilhamento de Dados</h3>
-                    <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
-                    
-                    <h3>4. Links para Sites Externos</h3>
-                    <p>Nosso site pode conter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
-                    
-                    <h3>5. Cookies e Publicidade</h3>
-                    <p>O serviço Google AdSense que usamos para veicular publicidade utiliza um cookie DoubleClick para exibir anúncios mais relevantes e limitar a frequência de exibição. Para mais informações, consulte as FAQs oficiais sobre privacidade do Google AdSense.</p>
-                    <p>Utilizamos anúncios para compensar os custos de funcionamento deste site e fornecer financiamento para futuros desenvolvimentos. Os cookies de publicidade comportamental rastreiam anonimamente seus interesses para apresentar anúncios relevantes.</p>
-                    
-                    <h3>6. Compromisso do Usuário</h3>
-                    <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Maktub Promotora oferece no site e não se envolver em atividades ilegais ou contrárias à boa fé e ordem pública.</p>
-                    <ul>
-                        <li>Não se envolver em atividades ilegais;</li>
-                        <li>Não difundir propaganda ou conteúdo ofensivo;</li>
-                        <li>Não causar danos aos sistemas físicos e lógicos do site.</li>
-                    </ul>
-                    
-                    <h3>7. Mais Informações</h3>
-                    <p>Se houver dúvidas sobre nossa política de privacidade, entre em contato conosco.</p>
-                </div>
-            </div> -->
         </section>
 
 
