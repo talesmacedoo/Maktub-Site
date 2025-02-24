750e2ca49b5acf438ba09d66e03ff71bfb6a791a
Tales Macedo <talesmgbahia@hotmail.com>
2025-02-24 15:14:38 -0300

    adicionando termo e condicoes

---
+++ termo_e_condicoes.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include 'head.php';
+    ?>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+</head>
+<body>
+
+    <header id="header" class="header fixed-top">
+
+        <?php
+        include 'header.php';
+        ?>
+
+    </header>
+
+    <main class="main">
+
+        <section id="top-section" class="top-section section accent-background">
+
+            <div class="container position-relative" data-aos="fade-up" data-aos-delay="10">
+                <div class="col-lg-12">
+                <img src="/maktub-site/assets/img/privacy-policy-pana.svg" alt="">
+                <div class="content text-center">
+                    <h2>Termos e Condições</h2>
+
+                </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="services" class="services section">
+
+            <div class="container">
+
+                <div class="content text-center">
+                    <h2>Termos e Condições</h2>
+                    
+                    <p>Os presentes Termos e Condições regem o uso do site Maktub Promotora. Ao acessá-lo, você concorda com as regras aqui estabelecidas, garantindo a conformidade com as leis e regulamentos aplicáveis. Caso não concorde com algum dos termos, recomendamos que cesse a utilização do site.</p>
+                </div>
+                <div class="row gy-4">
+
+
+                <div class="service-item  position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">                 
+
+                            <h3>1. Termos Gerais</h3>
+                        
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+
+                            <p>Ao acessar o site Maktub Promotora, você concorda em cumprir estes Termos de Serviço, todas as leis e regulamentos aplicáveis e assume a responsabilidade pelo cumprimento de todas as leis locais pertinentes. Caso não concorde com algum destes termos, está proibido de utilizar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">                       
+
+                            <h3>2. Uso da Licença</h3>
+                        
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                            <p>É concedida permissão para baixar temporariamente uma cópia dos materiais (informativos ou softwares) do site Maktub Promotora apenas para visualização pessoal e não comercial. Esta é uma concessão de licença, não uma transferência de propriedade. Sob esta licença, você não pode:</p>
+                            <ul>
+                                <li>Modificar ou copiar os materiais;</li>
+                                <li>Utilizar os materiais para qualquer finalidade comercial ou exibição pública;</li>
+                                <li>Tentar descompilar ou fazer engenharia reversa de qualquer software contido no site;</li>
+                                <li>Remover quaisquer direitos autorais ou outras notificações de propriedade;</li>
+                                <li>Transferir os materiais para outra pessoa ou "espelhar" os materiais em qualquer outro servidor.</li>
+                            </ul>
+                            <p>Esta licença será automaticamente rescindida caso você viole alguma destas restrições e poderá ser encerrada a qualquer momento pelo Maktub Promotora. Ao finalizar a visualização dos materiais ou após o encerramento desta licença, você deve apagar todos os materiais baixados, seja em formato eletrônico ou impresso.</p>
+
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                            <h3>3. Isenção de Responsabilidade</h3>
+                                                            
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+
+                            <p>Os materiais no site do Maktub Promotora são fornecidos "como estão". O Maktub Promotora não oferece garantias, expressas ou implícitas, e se isenta de todas as outras garantias, incluindo, sem limitação, garantias implícitas de comercialização, adequação a um fim específico ou não violação de direitos de propriedade intelectual ou outros direitos.</p>
+                            <p>Além disso, o Maktub Promotora não garante ou faz qualquer declaração quanto à precisão, aos resultados prováveis ou à confiabilidade do uso dos materiais em seu site ou de outra forma relacionados a esses materiais ou a sites vinculados a este.</p>
+
+                        </div>
+                    </div>
+                </div> 
+                
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">            
+
+                            <h3>4. Limitações de Responsabilidade</h3>
+                                                  
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                            <p>Em nenhum caso o Maktub Promotora ou seus fornecedores serão responsáveis por quaisquer danos (incluindo, sem limitação, danos por perda de dados, lucros cessantes ou interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais no site Maktub Promotora, mesmo que o Maktub Promotora ou um representante autorizado tenha sido notificado oralmente ou por escrito sobre a possibilidade de tais danos.</p>
+                            <p>Como algumas jurisdições não permitem limitações em garantias implícitas ou limitações de responsabilidade por danos consequenciais ou incidentais, essas limitações podem não se aplicar a você.</p>
+                        
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+                            <h3>5. Precisão dos Materiais</h3>
+                                                                
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+
+                            <p>Os materiais exibidos no site Maktub Promotora podem conter erros técnicos, tipográficos ou fotográficos. O Maktub Promotora não garante que qualquer material presente em seu site seja preciso, completo ou atualizado. Alteramos os materiais contidos no site a qualquer momento, sem aviso prévio, porém, não nos comprometemos a atualizá-los regularmente.</p>
+                            
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        
+                        <h3>6. Links para Sites Externos</h3>
+                                            
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+
+                            <p>O Maktub Promotora pode conter links para sites externos que não são operados por nós. Não nos responsabilizamos pelo conteúdo ou pelas políticas de privacidade de tais sites. A inclusão de qualquer link não implica endosso pelo Maktub Promotora. O uso de qualquer site vinculado é por conta e risco do usuário.</p>
+
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+                            <h3>7. Modificações dos Termos de Uso</h3>
+                                                
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">                       
+
+                            <p>O Maktub Promotora pode revisar estes Termos de Serviço a qualquer momento, sem aviso prévio. Ao continuar utilizando este site, você concorda em estar vinculado à versão atualizada destes termos.</p>
+                        
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="service-item position-relative">
+                    <div class="row gy-4">
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+
+                            <h3>8. Lei Aplicável</h3>
+                                                
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            
+
+                            <p>Estes Termos e Condições são regidos e interpretados de acordo com as leis vigentes na jurisdição do Maktub Promotora. Você se submete irrevogavelmente à jurisdição exclusiva dos tribunais competentes nessa localidade.</p>
+                            
+                        </div>
+                    </div>
+                </div> 
+
+
+                </div>
+
+            </div>
+
+            <!--
+            <div class="container position-relative" data-aos="fade-up" data-aos-delay="10">
+                <div class="content text-center">
+                    <h2>Política de Privacidade</h2>
+                    
+                    <p>A sua privacidade é importante para nós. É política do Maktub Promotora respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site Maktub Promotora, e outros sites que possuímos e operamos.</p>
+                </div>
+                
+                <div class="content">
+                    <div class="col-lg-6">
+                    <h3>1. Coleta e Uso de Informações</h3>
+                    </div>
+                    <div class="col-lg-6">
+                    <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
+                    </div>
+                    <h3>2. Retenção e Segurança dos Dados</h3>
+                    <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas, roubos, acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
+                    
+                    <h3>3. Compartilhamento de Dados</h3>
+                    <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
+                    
+                    <h3>4. Links para Sites Externos</h3>
+                    <p>Nosso site pode conter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
+                    
+                    <h3>5. Cookies e Publicidade</h3>
+                    <p>O serviço Google AdSense que usamos para veicular publicidade utiliza um cookie DoubleClick para exibir anúncios mais relevantes e limitar a frequência de exibição. Para mais informações, consulte as FAQs oficiais sobre privacidade do Google AdSense.</p>
+                    <p>Utilizamos anúncios para compensar os custos de funcionamento deste site e fornecer financiamento para futuros desenvolvimentos. Os cookies de publicidade comportamental rastreiam anonimamente seus interesses para apresentar anúncios relevantes.</p>
+                    
+                    <h3>6. Compromisso do Usuário</h3>
+                    <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Maktub Promotora oferece no site e não se envolver em atividades ilegais ou contrárias à boa fé e ordem pública.</p>
+                    <ul>
+                        <li>Não se envolver em atividades ilegais;</li>
+                        <li>Não difundir propaganda ou conteúdo ofensivo;</li>
+                        <li>Não causar danos aos sistemas físicos e lógicos do site.</li>
+                    </ul>
+                    
+                    <h3>7. Mais Informações</h3>
+                    <p>Se houver dúvidas sobre nossa política de privacidade, entre em contato conosco.</p>
+                </div>
+            </div> -->
+        </section>
+
+
+
+
+        <footer id="footer" class="footer accent-background">
+            <?php
+                include 'footer.php'
+            ?>
+
+        </footer>
+
+        <!-- Scroll Top -->
+        <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        <!-- Preloader -->
+        <div id="preloader"></div>
+
+        <!-- Vendor JS Files -->
+        <script src="/maktub-site/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/maktub-site/assets/vendor/php-email-form/validate.js"></script>
+        <script src="/maktub-site/assets/vendor/aos/aos.js"></script>
+        <script src="/maktub-site/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/maktub-site/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/maktub-site/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="/maktub-site/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+        <script src="/maktub-site/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+        <!-- Main JS File -->
+        <script src="/maktub-site/assets/js/main.js"></script>
+
+
+    </main>
+
+</body>
+</html>
