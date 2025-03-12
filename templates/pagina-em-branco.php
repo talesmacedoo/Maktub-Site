4c1cc2697fa9824be5490e9b17089faa274a76c5
Tales Macedo <talesmgbahia@hotmail.com>
2025-03-12 11:12:48 -0300

    Criando pasta para templates

---
+++ templates/pagina-em-branco.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include '../head.php';
+    ?>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+</head>
+<body>
+
+    <header id="header" class="header fixed-top">
+
+        <?php
+        include '../header.php';
+        ?>
+
+    </header>
+    <main class="main">
+        <section id="top-section" class="top-section section accent-background">
+
+        </section>
+
+
+
+        <footer id="footer" class="footer accent-background">
+            <?php
+                include '../footer.php'
+            ?>
+
+        </footer>
+
+        <!-- Scroll Top -->
+        <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        <!-- Preloader -->
+        <div id="preloader"></div>
+
+        <!-- Vendor JS Files -->
+        <script src="/maktub-site/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/maktub-site/assets/vendor/php-email-form/validate.js"></script>
+        <script src="/maktub-site/assets/vendor/aos/aos.js"></script>
+        <script src="/maktub-site/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/maktub-site/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/maktub-site/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="/maktub-site/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+        <script src="/maktub-site/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+        <!-- Main JS File -->
+        <script src="/maktub-site/assets/js/main.js"></script>
+
+
+    </main>
+
+</body>
+</html>
